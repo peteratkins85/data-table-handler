ffaee86800389d9b2fd3aa8e205624e66f42470d
Peter Atkins <peter.atkins@eagleeye.com>
2017-08-09 12:38:47 +0100

    Added doc blocks to DataTableQueryManagerInterface methods

--- lib/DataTableQueryManagerInterface.php
+++ lib/DataTableQueryManagerInterface.php
@@ -8,9 +8,31 @@ namespace ShinraCoder\DataTableHandler;
  */
 interface DataTableQueryManagerInterface
 {
+    /**
+     * Expected multidimensional array row results e.g.
+     * [
+     *      [
+     *          'field1',
+     *          'field2',
+     *          'field3,
+     *      ]
+     * ]
+     * @param DataTable $dataTable
+     * @return mixed
+     */
     public function queryData(DataTable $dataTable);
 
+    /**
+     * The DataTable class will use this method to set
+     * its property "RecordsTotal"
+     * @return mixed
+     */
     public function getResultTotal();
 
+    /**
+     * The DataTable class will use this method to set
+     * its property "RecordsFiltered"
+     * @return mixed
+     */
     public function getFilteredResultTotal();
 }
